ad5f24ea399bac6618d652d3e65bb5259626f3cc
Nick <nackolas18@gmail.com>
2022-07-02 22:36:19 -0400

    https://www.youtube.com/watch?v=CV_mMAYzTxw
    md5 crack using dictionary

---
+++ PasswordCracker.py
@@ -0,0 +1,36 @@
+import hashlib
+
+flag = 0
+counter = 0
+
+pass_hash = input("Enter md5 hash: ")
+
+wordlist = input("File name: ")
+
+try:
+    pass_file = open(wordlist, "r")
+except:
+    print("No file found")
+    quit()
+
+for word in pass_file:
+
+    enc_wrd = word.encode('utf-8')
+    digest = hashlib.md5(enc_wrd.strip()).hexdigest()
+
+    #print(word)
+    #print(digest)
+    #print(pass_hash)
+
+    counter = counter+1
+
+    if digest == pass_hash:
+        print("Password found")
+        print("Password is " + word)
+        flag = 1
+        print(f"password found in file '{pass_file.name}' at line: {counter}")
+        break
+
+
+if flag == 0:
+    print("Password/passphrase is not in the list")
